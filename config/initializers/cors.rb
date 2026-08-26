5ca4c2c3210451dc0ebdef537c76ec61a4ccedf7
rfusco22 <fuscoriccardo11@gmail.com>
2026-08-25 23:48:52 -0400

    fix(security): restringir methods y headers en CORS
    - /public/api/*: methods [:get, :post, :options] en vez de :any
    - Todas las rutas: headers whitelist ['Content-Type', 'Authorization']
    - Previene requests destructivos y cache poisoning
    
    Closes #18, #19

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -12,15 +12,15 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     resource '/packs/*', headers: :any, methods: [:get, :options]
     resource '/audio/*', headers: :any, methods: [:get, :options]
     # Make the public endpoints accessible to the frontend
-    resource '/public/api/*', headers: :any, methods: :any
+    resource '/public/api/*', headers: ['Content-Type', 'Authorization'], methods: [:get, :post, :options]
 
     if ActiveModel::Type::Boolean.new.cast(ENV.fetch('CW_API_ONLY_SERVER', false)) || Rails.env.development?
       origins(*allowed_origins, localhost: nil, /127\.0\.0\.1/)
-      resource '*', headers: :any, methods: :any, expose: %w[access-token client uid expiry]
+      resource '*', headers: ['Content-Type', 'Authorization', 'X-Auth-Token'], methods: :any, expose: %w[access-token client uid expiry]
     end
 
     if ActiveModel::Type::Boolean.new.cast(ENV.fetch('ENABLE_API_CORS', false))
-      resource '/api/*', headers: :any, methods: :any, expose: %w[access-token client uid expiry]
+      resource '/api/*', headers: ['Content-Type', 'Authorization', 'X-Auth-Token'], methods: :any, expose: %w[access-token client uid expiry]
     end
   end
 end
